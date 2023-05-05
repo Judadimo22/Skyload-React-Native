727399659491c7460aea1fa4b84dccac2b6bd48b
Judadimo22 <109996961+Judadimo22@users.noreply.github.com>
2023-05-04 19:58:38 -0500

    auth0 funcionando

--- client/App.js
+++ client/App.js
@@ -1,16 +1,68 @@
+import React from 'react';
+import {Alert, Button, StyleSheet, Text, View} from 'react-native';
+import {useAuth0, Auth0Provider} from 'react-native-auth0';
+import config from './auth0-configuration';
 
-import React from "react";
-import {View, StyleSheet,Text } from "react-native";
-import Navigation from "./Navigation";
+const Home = () => {
+  const {authorize, clearSession, user, error, getCredentials} = useAuth0();
 
+  const onLogin = async () => {
+    try {
+      await authorize({scope: 'openid profile email'}, {customScheme: 'auth0.com.auth0samples'});
+      let credentials = await getCredentials();
+      Alert.alert('AccessToken: ' + credentials.accessToken);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
+  const loggedIn = user !== undefined && user !== null;
 
+  const onLogout = async () => {
+    try {
+      await clearSession({customScheme: 'auth0.com.auth0samples'});
+    } catch (e) {
+      console.log('Log out cancelled');
+    }
+  };
 
-export default function App() {
   return (
-    <Navigation/>
+    <View style={styles.container}>
+      <Text style={styles.header}> Auth0Sample - Login </Text>
+      {user && <Text>You are logged in as {user.email}</Text>}
+      {!user && <Text>You are not logged in</Text>}
+      {error && <Text>{error.message}</Text>}
+      <Button
+        onPress={loggedIn ? onLogout : onLogin}
+        title={loggedIn ? 'Log Out' : 'Log In'}
+      />
+    </View>
   );
 };
+
+const App = () => {
+  return (
+    <Auth0Provider domain={config.domain} clientId={config.clientId}>
+      <Home />
+    </Auth0Provider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default App;
 
 
 
---
+++ client/auth0-configuration.js
@@ -0,0 +1,6 @@
+const config = {
+    clientId: "7JB1B7pQ7O5ppCWOQrdOPVCpCTmj7ls0",
+    domain: "dev-05o0jj0rko2ty63h.us.auth0.com"
+  };
+  
+  export default config;
